4aacd151d07e94221b14d8dde908833cfd93f68f
YangJ0605 <1442122744@qq.com>
2019-10-30 18:48:18 +0800

    源码

---
+++ src/components/footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <p>© &nbsp;&nbsp;一个基于vue完成的多人共享博客   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-by mo白</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+
+</style>
